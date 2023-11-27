29fa998745e8ce695c20c04250e988b9a95c299f
luiz felipe <cruzsantosluizfelipe@gmail.com>
2023-11-27 00:09:00 -0300

    criarValidacoes

--- scripts/create-quiz.js
+++ scripts/create-quiz.js
@@ -11,15 +11,28 @@ function validaCampos() {
 
     if (titulo == '' || url == '' || nivel == '' || qtdePerguntas == '') {
         alert(msgError);
-    } else {
+
+    }
+
+    else if (titulo.length < 20 || titulo.length > 65) {
+        alert('O título deve conter no mínimo 20 caracteres e no máximo 65 caracteres.');
+    }
+    else if (qtdePerguntas < 3) {
+        alert('A quantidade de perguntas precisa ser maior que 3');
+    }
+    else if (nivel < 2) {
+        alert('O nível deve ser no minimo 2');
+    }
+
+    else {
         // Faça algo com os valores dos campos
         // Construir a URL com parâmetros de consulta
-        var urlDestino = 'create-question.html' + 
-            '?var1=' + encodeURIComponent(titulo) + 
-            '&var2=' + encodeURIComponent(url) + 
-            '&var3=' + encodeURIComponent(nivel) +
-            '&var4=' + encodeURIComponent(qtdePerguntas);
+        localStorage.setItem('var1', titulo);
+        localStorage.setItem('var2', url);
+        localStorage.setItem('var3', nivel);
+        localStorage.setItem('var4', qtdePerguntas);
 
         // Redirecionar para a próxima página
-        window.location.href = urlDestino;    }
+        window.location.href = '../pagina2.html';
+    }
 }
